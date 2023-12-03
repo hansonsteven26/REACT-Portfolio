b084593788ea18dabd9278e41c8424986111a566
steven <smhanson21@gmail.com>
2023-12-03 17:21:23 -0600

    add contact me

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,159 @@
-import { Box, Typography } from '@mui/material';
+import {
+  Box,
+  Card,
+  CardContent,
+  Grid,
+  TextField,
+  Typography,
+} from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { useState } from 'react';
+import emailjs from '@emailjs/browser';
+import { validateEmail } from '../utils/helpers';
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    name: '',
+    message: '',
+  });
+  const [responseMessage, setResponseMessage] = useState({
+    message: '',
+    type: '',
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleBlur = (e) => {
+    if (e.target.name == 'name' && !formData.name) {
+      setResponseMessage({
+        message: 'A name is required',
+        type: 'warning',
+      });
+    }
+
+    if (e.target.name == 'email' && !formData.email) {
+      setResponseMessage({
+        message: 'An email is required',
+        type: 'warning',
+      });
+    }
+
+    if (e.target.name == 'message' && !formData.message) {
+      setResponseMessage({
+        message: 'A message is required',
+        type: 'warning',
+      });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let form = document.getElementById('contact-me-form');
+
+    if (!formData.email || !formData.message) {
+      setResponseMessage({ message: 'Error submitting form', type: 'error' });
+      return;
+    }
+    if (!validateEmail(formData.email)) {
+      setResponseMessage({
+        message: 'Invalid email',
+        type: 'error',
+      });
+      return;
+    }
+
+    setLoading(true);
+
+    emailjs
+      .sendForm(
+        'service_7pb3op9',
+        'template_6rlkit6',
+        form,
+        'qqvh9l_k9DDGWWdkf'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setResponseMessage({ message: '', type: '' });
+          setLoading(false);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
-    <Box id="contact">
-      <Typography variant="h2">Contact</Typography>
+    <Box className="contact-me" id="contact">
+      <Typography variant="h2" margin={3} textAlign={'center'}>
+        Contact Me
+      </Typography>
+      <Grid container justifyContent={'center'}>
+        <Grid item sm={10} md={10} lg={7} textAlign={'center'}>
+          <Card>
+            <CardContent>
+              <form id="contact-me-form">
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  justifyContent="center"
+                  alignItems="center"
+                  margin={3}
+                >
+                  <TextField
+                    id="name-form"
+                    label="Your Name"
+                    name="name"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    margin="dense"
+                    fullWidth
+                  />
+                  <TextField
+                    id="email-form"
+                    label="Your Email"
+                    name="email"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    margin="dense"
+                    fullWidth
+                  />
+                  <TextField
+                    id="message-form"
+                    label="Your Message Here"
+                    multiline
+                    rows={4}
+                    name="message"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    margin="dense"
+                    fullWidth
+                  />
+                </Box>
+                <Typography>
+                  {responseMessage
+                    ? responseMessage.message
+                    : console.log(responseMessage)}
+                </Typography>
+              </form>
+              <LoadingButton
+                onClick={handleSubmit}
+                endIcon={<SendIcon />}
+                loading={loading}
+                loadingPosition="end"
+                variant="contained"
+              >
+                <span>Send</span>
+              </LoadingButton>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
