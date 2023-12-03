74109187545cb0dbd0354dbe3ec0a6b34bfaa571
steven <smhanson21@gmail.com>
2023-12-03 01:54:46 -0600

    add about component

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from '@mui/material';
+
+const About = () => {
+  return (
+    <Box id="about">
+      <Typography variant="h2">About Me</Typography>
+    </Box>
+  );
+};
+
+export default About;
