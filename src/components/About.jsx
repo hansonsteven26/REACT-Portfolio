71129788783ba8f4ddc2640bf66e052d450e5dae
steven <smhanson21@gmail.com>
2023-12-03 17:57:20 -0600

    change about me description

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,14 +18,20 @@ const About = () => {
       </Typography>
       <Box sx={{ textAlign: 'center' }} width={'60vw'}>
         <Card>
-          <CardMedia component="img" image={Me} height={100} sx={{ objectFit: 'contain' }} />
+          <CardMedia
+            component="img"
+            image={Me}
+            height={100}
+            sx={{ objectFit: 'contain' }}
+          />
           <CardContent>
             <Typography>
-              I am a full stack web developer with a background in sales and
-              customer service. I have a passion for learning and problem
-              solving,and I am a team player who enjoys collaborating with
-              others. I am looking forward to working with a team of developers
-              to create amazing applications.
+              Motivated Web Developer with strong problem-solving skills, and
+              exceptional attention to detail seeking technology roles. I have
+              experience with multiple coding languages, a strong ability to
+              learn new concepts easily and make an excellent team player. Able
+              to adapt quickly to shifting priorities and ongoing changes in
+              work.
             </Typography>
           </CardContent>
         </Card>
