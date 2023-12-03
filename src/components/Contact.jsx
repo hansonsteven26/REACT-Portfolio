0a5aff87887834653b295c859db9b647ce1c863d
steven <smhanson21@gmail.com>
2023-12-03 02:05:27 -0600

    add contact.jsx

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from '@mui/material';
+
+const Contact = () => {
+  return (
+    <Box id="contact">
+      <Typography variant="h2">Contact</Typography>
+    </Box>
+  );
+};
+
+export default Contact;
