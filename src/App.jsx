d50342fa7072e0f242afe7b1a2e03308191df187
steven <smhanson21@gmail.com>
2023-12-03 02:05:48 -0600

    render Contact component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Resume from './components/Resume';
 import Footer from './components/Footer';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <About />
       <Portfolio />
       <Resume />
+      <Contact />
       <Footer />
     </div>
   );
